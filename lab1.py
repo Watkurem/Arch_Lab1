d7e20c040e09e05b60379fc7d92585be9f529cd7
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-03-01 18:00:49 +0200

    Added license and another test, yes.

---
+++ lab1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Copyright 2015 Alexander Melnyk / Олександр Мельник
+#
+# This file is part of Arch_Lab1 package.
+#
+# Arch_Lab1 is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Arch_Lab1 is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Arch_Lab1. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+def main():
+    print("Lol got ya!")
+
+if __name__=="__main__":
+   main()

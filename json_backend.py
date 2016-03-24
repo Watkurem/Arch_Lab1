7e52a7be24e777c5f6f870052ffa51ec0fcc51de
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-03-24 20:10:20 +0200

    Add serialization backend for json
    One unruly bastard. Turns out, JSON Python interface doesn't know how to
    save anything more complex than a dict. Turns out, that's the intended
    way and yours truly has to write his own implementation of JSONEncoder
    for every class he wishes to serialize. And decoder too, but it's
    architecture is much worse and I don't have time to investigate it.
    
    Works only with one 'engine.Task' implementation hardcoded into it. Can
    be extended to be more flexible, but you know what? Python devs
    should've done that.

--- json_backend.py
+++ json_backend.py
@@ -18,3 +18,36 @@
 # You should have received a copy of the GNU General Public License along with
 # Arch_Lab1. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
+
+import json
+import engine
+import datetime
+
+class TaskJSONEncoder(json.JSONEncoder):
+    def default (self, obj):
+        if isinstance(obj, engine.Task):
+            return {"__engine.Task__": True, **obj.__dict__}
+        if isinstance(obj, datetime.date):
+            return (obj.year, obj.month, obj.day)
+
+def save(target, item):
+    """
+    """
+    with open(target, 'w') as fil:
+        json.dump(item, fil, cls = TaskJSONEncoder)
+
+def load(target):
+    """
+    """
+    try:
+        with open(target, 'r') as fil:
+            tmp1, tmp2 = json.load(fil)
+            tmp1 = [engine.Task(x['content'],
+                                x['date'][0], x['date'][1], x['date'][2])
+                    for x in tmp1 if '__engine.Task__' in x.keys()]
+            tmp2 = [engine.Task(x['content'],
+                                x['date'][0], x['date'][1], x['date'][2])
+                    for x in tmp2 if '__engine.Task__' in x.keys()]
+            return (tmp1, tmp2)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return ([], [])

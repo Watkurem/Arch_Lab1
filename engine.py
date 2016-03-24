291c0367cdda2460dd595b4939244cdd9641f4d6
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-03-24 22:00:45 +0200

    Update Arch_Lab1 naming to better Arch_Lab

--- engine.py
+++ engine.py
@@ -3,25 +3,25 @@
 ###############################################################################
 # Copyright 2016 Alexander Melnyk / Олександр Мельник
 #
-# This file is part of Arch_Lab1 package.
+# This file is part of Arch_Lab package.
 #
-# Arch_Lab1 is free software: you can redistribute it and/or modify it under
+# Arch_Lab is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# Arch_Lab1 is distributed in the hope that it will be useful, but WITHOUT
+# Arch_Lab is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# Arch_Lab1. If not, see <http://www.gnu.org/licenses/>.
+# Arch_Lab. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-"""Arch_Lab1 simple list engine.
+"""Arch_Lab simple list engine.
 
-This module is an engine ("model") for the Arch_Lab1 program. You probably
+This module is an engine ("model") for the Arch_Lab program. You probably
 should not be importing it directly.
 
 pending_task_list: list of pending tasks.
--- yaml_backend.py
+++ yaml_backend.py
@@ -3,20 +3,20 @@
 ###############################################################################
 # Copyright 2016 Alexander Melnyk / Олександр Мельник
 #
-# This file is part of Arch_Lab1 package.
+# This file is part of Arch_Lab package.
 #
-# Arch_Lab1 is free software: you can redistribute it and/or modify it under
+# Arch_Lab is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# Arch_Lab1 is distributed in the hope that it will be useful, but WITHOUT
+# Arch_Lab is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# Arch_Lab1. If not, see <http://www.gnu.org/licenses/>.
+# Arch_Lab. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 import yaml

896408b03805fc08d6d18227289b77a6b284d31f
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-03-24 22:02:39 +0200

    Add docstrings and doctests
    Docstrings - everywhere, doctests - where applicable.

--- pickle_backend.py
+++ pickle_backend.py
@@ -19,16 +19,41 @@
 # Arch_Lab. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
+"""Arch_Lab pickle serialization backend.
+
+This module is a serialization backend for the Arch_Lab program. It exists to
+provide unified serialization interface to pickle for engine. You probably
+should not be importing it directly.
+"""
+
 import pickle
 
 def save(target, item):
-    """
+    """Serialize item into filename target. Create file or overwrite.
+
+    target: string - file name.
+    item: any python data structure - item to serialize.
+
+    >>> import tempfile; import engine; \
+    tmp = tempfile.NamedTemporaryFile(); \
+    control = ([engine.Task("A", 1, 1, 1)], [engine.Task("B", 1000, 2, 3)]); \
+    save(tmp.name, control); \
+    tester = load(tmp.name); \
+    control == tester
+    True
     """
     with open(target, 'wb') as fil:
         pickle.dump(item, fil)
 
 def load(target):
-    """
+    """Deserialize filename target.
+
+    Will return tuple of two empty lists if file does not exist.
+
+    target: string - file name.
+    return: any Python data structure or ([], [])
+
+    Tested in save()
     """
     try:
         with open(target, 'rb') as fil:
--- yaml_backend.py
+++ yaml_backend.py
@@ -19,16 +19,41 @@
 # Arch_Lab. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
+"""Arch_Lab YAML serialization backend.
+
+This module is a serialization backend for the Arch_Lab program. It exists to
+provide unified serialization interface to pyyaml for engine. You probably
+should not be importing it directly.
+"""
+
 import yaml
 
 def save(target, item):
-    """
+    """Serialize item into filename target. Create file or overwrite.
+
+    target: string - file name.
+    item: any python data structure - item to serialize.
+
+    >>> import tempfile; import engine; \
+    tmp = tempfile.NamedTemporaryFile(mode="r+"); \
+    control = ([engine.Task("A", 1, 1, 1)], [engine.Task("B", 1000, 2, 3)]); \
+    save(tmp.name, control); \
+    tester = load(tmp.name); \
+    control == tester
+    True
     """
     with open(target, 'w') as fil:
         yaml.dump(item, fil)
 
 def load(target):
-    """
+    """Deserialize filename target.
+
+    Will return tuple of two empty lists if file does not exist.
+
+    target: string - file name.
+    return: any Python data structure or ([], [])
+
+    Tested in save()
     """
     with open(target, 'r') as fil:
         test = yaml.load(fil)

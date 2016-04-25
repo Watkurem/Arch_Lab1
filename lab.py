d97f1c5b18a58d4cb2b015675d920393c1f0183b
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-04-25 19:42:18 +0300

    Remove controller code from lab.py

--- lab.py
+++ lab.py
@@ -36,228 +36,54 @@ import engine
 import sys
 
 
-def main():
-    """Entry point for program.
-
-    Displays welcome message and switches to pending task view.
-    Start Arch_Lab by calling this function.
-    """
-    interface.welcome()
-    view_pending_tasks()
 
 
-def view_pending_tasks():
-    """Provides interactive view of pending tasks.
-
-    Tasks will be sorted by date from earliest to latest; tasks that are
-    overdue (dated earlier than current date, but still pending) and tasks
-    scheduled for current date will be marked accordingly.
-    """
-    PENDING_TASK_OPTS = (
-        ("N", "Add new task", add_new_task),
-        ("R", "Remove task", remove_pending_task),
-        ("E", "Edit task", edit_pending_task),
-        ("M", "Mark task finished", finish_task),
-        ("F", "View finished tasks", view_finished_tasks),
-        ("C", "Edit configuration", view_config_pending),
-        ("Q", "Quit", shutdown)
-    )
-
-    interface.print_pending_tasks(engine.view_pending_tasks())
-    try:
-        interface.pending_tasks_menu(PENDING_TASK_OPTS)[2]()
-    except TypeError:
-        interface.bad_input()
-        view_pending_tasks()
 
 
-def add_new_task():
-    """Add new task interactively.
-
-    Task will be marked pending by default. After task is added returns to the
-    pending task view.
-    """
-    try:
-        engine.new_task(*interface.new_task_dialog())
-    except (TypeError, ValueError):
-        interface.bad_input()
-
-    view_pending_tasks()
 
 
-def remove_pending_task():
-    """Provides interactive way to remove one pending task.
-
-    After task is removed returns to the pending task view.
-    """
-    try:
-        engine.remove_pending_task(interface.ask_task())
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-
-    view_pending_tasks()
 
 
-def edit_pending_task():
-    """Provides interactive way to edit one pending task.
-
-    After edits are done returns to the pending task view.
-    """
-    choice = interface.ask_task()
-    args = interface.edit_task_dialog(choice)
-
-    try:
-        engine.edit_pending_task(choice, *args)
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-    except ValueError:
-        interface.bad_input()
-
-    view_pending_tasks()
 
 
-def finish_task():
-    """Mark pending task as finished interactively.
-
-    Afterwards returns to the pending task view.
-    """
-
-    try:
-        engine.finish_task(interface.ask_task())
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-
-    view_pending_tasks()
 
 
-def view_config_pending():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
     """
-    view_config()
-    view_pending_tasks()
-
-
-def view_finished_tasks():
-    """Provides interactive view of finished tasks.
-
-    Tasks will be sorted by date from earliest to latest.
-    """
-    FINISHED_TASK_OPTS = (
-        ("W", "Wipe finished tasks", clear_finished_tasks),
-        ("R", "Remove task", remove_finished_task),
-        ("E", "Edit task", edit_finished_task),
-        ("M", "Mark task pending", unfinish_task),
-        ("L", "View pending tasks", view_pending_tasks),
-        ("C", "Edit configuration", view_config_finished),
-        ("Q", "Quit", shutdown)
-    )
-
-    interface.print_finished_tasks(engine.view_finished_tasks())
-    try:
-        interface.finished_tasks_menu(FINISHED_TASK_OPTS)[2]()
-    except TypeError:
-        interface.bad_input()
-        view_finished_tasks()
-
-
-def clear_finished_tasks():
-    """Remove all finished tasks.
-
-    Afterwards returns to the finished task view.
-    """
-    engine.clear_finished_tasks()
-    view_finished_tasks()
-
-
-def remove_finished_task():
-    """Provides interactive way to remove one finished task.
-
-    After task is removed returns to the finished task view.
-    """
-
-    try:
-        engine.remove_finished_task(interface.ask_task())
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-
-    view_finished_tasks()
-
-
-def edit_finished_task():
-    """Provides interactive way to edit one finished task.
-
-    After edits are done returns to the finished task view.
-    """
-    choice = interface.ask_task()
-    args = interface.edit_task_dialog(choice)
-
-    try:
-        engine.edit_finished_task(choice, *args)
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-    except ValueError:
-        interface.bad_input()
-
-    view_finished_tasks()
-
-
-def unfinish_task():
-    """Mark finished task as pending.
-
-    Afterwards returns to the finished task view.
-    """
-    try:
-        engine.unfinish_task(interface.ask_task())
-    except TypeError:
-        pass
-    except IndexError:
-        interface.bad_task()
-
-    view_finished_tasks()
-
-
-def view_config_finished():
-    """
-    """
-    view_config()
-    view_finished_tasks()
-
-
-def shutdown():
-    """
-    """
-    # try:
-    save_dialog()
-    # except engine.SaveFail:
-    #     interface.no_save_method_error()
-    #     view_config()
-    # else:
-    sys.exit()
-
-
-def save_dialog():
-    """
-    """
-    if engine.changes_detected() and interface.save_dialog():
-        engine.save_tasks()
-
-
-def view_config():
-    """
-    """
-    tmp = interface.config_menu(engine.get_savemethod(),
-                                engine.get_available_savemethods())
-    engine.set_savemethod(tmp)
 
 
 if __name__ == "__main__":

4355319483d681ca5cd4b6294d5df6e3dbda89d1
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-04-25 20:05:43 +0300

    Update docstring

--- lab.py
+++ lab.py
@@ -20,15 +20,10 @@
 # Arch_Lab. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-"""Arch_Lab main module ("controller"). Run this as a script or import and
-run main().
+"""Arch_Lab main module. Run this as a script or import and run main().
 
-All functions are interactive and probably should not be run directly (only via
-main()). Exact behaviour of each function depends on interface ("view") and
-engine ("model") that implement them, but an attempt was made to describe
-generally expected behaviour.
-
-'Pending' means that task is not yet done. Finished means that task is done.
+Module itself provides abstract classes/interfaces for the program's 'building
+blocks' - controller, interface ('view' in MVC) and engine ('model' in MVC).
 """
 
 

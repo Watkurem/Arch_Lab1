ef3384c2dff5c816afd9ef7824657bb95d19d898
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-04-25 20:36:43 +0300

    Update docstring

--- json_backend.py
+++ json_backend.py
@@ -21,9 +21,8 @@
 
 """Arch_Lab JSON serialization backend.
 
-This module is a serialization backend for the Arch_Lab program. It exists to
-provide unified serialization interface to json for engine. You probably
-should not be importing it directly.
+This module provides a serialization backend for the Arch_Lab program. You
+probably should not be importing it directly.
 """
 
 import json
